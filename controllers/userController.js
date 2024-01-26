c6856d7d046077e7e0aa5a4f8073261af61d3d14
Neal Chris <tmbarnhart1@gmail.com>
2024-01-25 20:51:09 -0500

    Rework to the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,10 @@
-// Imports the 'User' and 'Thought' models from the "../models" directory
 const { User, Thought } = require("../models");
 
-// Controller for handling CRUD operations related to users
 const userController = {
-  // Retrieves all users from the database, populating thoughts and friends for each user
+  // Get all users
   async getUsers(req, res) {
     try {
-      const users = await User.find()
-        .populate({ path: "thoughts", select: "-__v" })
-        .populate({ path: "friends", select: "-__v" });
-
+      const users = await User.find().select("-__v");
       return res.status(200).json(users);
     } catch (err) {
       console.log(err);
@@ -17,12 +12,13 @@ const userController = {
     }
   },
 
-  // Retrieves a single user based on the provided user ID, populating thoughts and friends
+  // Get a single user by ID
   async getUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId })
-        .populate({ path: "thoughts", select: "-__v" })
-        .populate({ path: "friends", select: "-__v" });
+        .select("-__v")
+        .populate("friends")
+        .populate("thoughts");
 
       if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
@@ -35,7 +31,7 @@ const userController = {
     }
   },
 
-  // Creates a new user
+  // Create a new user
   async createUser(req, res) {
     try {
       const user = await User.create(req.body);
@@ -46,13 +42,13 @@ const userController = {
     }
   },
 
-  // Updates an existing user based on the provided user ID
+  // Update an existing user by ID
   async updateUser(req, res) {
     try {
       const user = await User.findOneAndUpdate(
         { _id: req.params.userId },
         { $set: req.body },
-        { runValidators: true, new: true }
+        { new: true, runValidators: true }
       );
 
       if (!user) {
@@ -66,7 +62,7 @@ const userController = {
     }
   },
 
-  // Deletes a user based on the provided user ID, along with associated thoughts
+  // Delete a user by ID
   async deleteUser(req, res) {
     try {
       const user = await User.findOneAndDelete({ _id: req.params.userId });
@@ -75,11 +71,11 @@ const userController = {
         return res.status(404).json({ message: "No user with that ID" });
       }
 
-      // Deletes thoughts associated with the user
+      // Delete thoughts associated with the user
       await Thought.deleteMany({ _id: { $in: user.thoughts } });
 
       return res.status(200).json({
-        message: "User and associated thoughts and reactions deleted!",
+        message: "User and associated thoughts deleted",
       });
     } catch (err) {
       console.log(err);
@@ -87,40 +83,40 @@ const userController = {
     }
   },
 
-  // Adds a friend to a user's friend list
+  // Add a friend to a user's friend list
   async addFriend(req, res) {
     try {
-      const friend = await User.findOneAndUpdate(
+      const user = await User.findOneAndUpdate(
         { _id: req.params.userId },
         { $addToSet: { friends: req.params.friendId } },
-        { runValidators: true, new: true }
+        { new: true }
       );
 
-      if (!friend) {
+      if (!user) {
         return res.status(404).json({ message: "No user with that ID" });
       }
 
-      return res.status(200).json(friend);
+      return res.status(200).json(user);
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
     }
   },
 
-  // Deletes a friend from a user's friend list
+  // Delete a friend from a user's friend list
   async deleteFriend(req, res) {
     try {
-      const friend = await User.findOneAndUpdate(
+      const user = await User.findOneAndUpdate(
         { _id: req.params.userId },
         { $pull: { friends: req.params.friendId } },
-        { runValidators: true, new: true }
+        { new: true }
       );
 
-      if (!friend) {
-        return res.status(404).json({ message: "Check user and friend ID" });
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
       }
 
-      return res.status(200).json(friend);
+      return res.status(200).json(user);
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
@@ -128,5 +124,4 @@ const userController = {
   },
 };
 
-// Exports the userController for use in other parts of the application
 module.exports = userController;
